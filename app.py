bbf08ed714416dae759c71fa8b926822954e4f3c
piyush8884 <gg6480@srmist.edu.in>
2023-08-17 12:29:42 +0530

    Commit_all_files

---
+++ app.py
@@ -0,0 +1,134 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+app = Flask(__name__)
+
+l1 = ['itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 'joint_pain',
+      'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 'burning_micturition',
+      'spotting_ urination', 'fatigue',
+      'weight_gain', 'anxiety', 'cold_hands_and_feets', 'mood_swings', 'weight_loss', 'restlessness', 'lethargy',
+      'patches_in_throat',
+      'irregular_sugar_level', 'cough', 'high_fever', 'sunken_eyes', 'breathlessness', 'sweating', 'dehydration',
+      'indigestion',
+      'headache', 'yellowish_skin', 'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 'back_pain',
+      'constipation',
+      'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine', 'yellowing_of_eyes', 'acute_liver_failure',
+      'fluid_overload',
+      'swelling_of_stomach', 'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm',
+      'throat_irritation',
+      'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs',
+      'fast_heart_rate',
+      'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus', 'neck_pain',
+      'dizziness', 'cramps',
+      'bruising', 'obesity', 'swollen_legs', 'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid',
+      'brittle_nails',
+      'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips', 'slurred_speech',
+      'knee_pain', 'hip_joint_pain',
+      'muscle_weakness', 'stiff_neck', 'swelling_joints', 'movement_stiffness', 'spinning_movements', 'loss_of_balance',
+      'unsteadiness', 'weakness_of_one_body_side',
+      'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine', 'continuous_feel_of_urine', 'passage_of_gases',
+      'internal_itching', 'toxic_look_(typhos)',
+      'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 'belly_pain',
+      'abnormal_menstruation', 'dischromic _patches',
+      'watering_from_eyes', 'increased_appetite', 'polyuria', 'family_history', 'mucoid_sputum', 'rusty_sputum',
+      'lack_of_concentration', 'visual_disturbances',
+      'receiving_blood_transfusion', 'receiving_unsterile_injections', 'coma', 'stomach_bleeding',
+      'distention_of_abdomen', 'history_of_alcohol_consumption',
+      'fluid_overload', 'blood_in_sputum', 'prominent_veins_on_calf', 'palpitations', 'painful_walking',
+      'pus_filled_pimples', 'blackheads', 'scurring', 'skin_peeling',
+      'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister', 'red_sore_around_nose',
+      'yellow_crust_ooze']
+# List of symptoms
+disease = ['Fungal infection', 'Allergy', 'GERD', 'Chronic cholestasis', 'Drug Reaction',
+           'Peptic ulcer diseae', 'AIDS', 'Diabetes', 'Gastroenteritis', 'Bronchial Asthma', 'Hypertension',
+           ' Migraine', 'Cervical spondylosis',
+           'Paralysis (brain hemorrhage)', 'Jaundice', 'Malaria', 'Chicken pox', 'Dengue', 'Typhoid', 'hepatitis A',
+           'Hepatitis B', 'Hepatitis C', 'Hepatitis D', 'Hepatitis E', 'Alcoholic hepatitis', 'Tuberculosis',
+           'Common Cold', 'Pneumonia', 'Dimorphic hemmorhoids(piles)',
+           'Heartattack', 'Varicoseveins', 'Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia', 'Osteoarthristis',
+           'Arthritis', '(vertigo) Paroymsal  Positional Vertigo', 'Acne', 'Urinary tract infection', 'Psoriasis',
+           'Impetigo']
+# List of diseases
+l2 = [0] * len(l1)
+
+# Load and preprocess testing data
+tr = pd.read_csv("C:/Users/ADMIN/Downloads/Diseaseprediction-master/Diseaseprediction-master/Testing.csv")
+# Replace disease labels with numerical values
+tr.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
+'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
+'Migraine':11,'Cervical spondylosis':12,
+'Paralysis (brain hemorrhage)':13,'Jaundice':14,'Malaria':15,'Chicken pox':16,'Dengue':17,'Typhoid':18,'hepatitis A':19,
+'Hepatitis B':20,'Hepatitis C':21,'Hepatitis D':22,'Hepatitis E':23,'Alcoholic hepatitis':24,'Tuberculosis':25,
+'Common Cold':26,'Pneumonia':27,'Dimorphic hemmorhoids(piles)':28,'Heart attack':29,'Varicose veins':30,'Hypothyroidism':31,
+'Hyperthyroidism':32,'Hypoglycemia':33,'Osteoarthristis':34,'Arthritis':35,
+'(vertigo) Paroymsal  Positional Vertigo':36,'Acne':37,'Urinary tract infection':38,'Psoriasis':39,
+'Impetigo':40}},inplace=True)
+X_test = tr[l1]
+y_test = tr[["prognosis"]]
+
+# Load and preprocess training data
+df = pd.read_csv("C:/Users/ADMIN/Downloads/Diseaseprediction-master/Diseaseprediction-master/Training.csv")
+# Replace disease labels with numerical values
+df.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
+'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
+'Migraine':11,'Cervical spondylosis':12,
+'Paralysis (brain hemorrhage)':13,'Jaundice':14,'Malaria':15,'Chicken pox':16,'Dengue':17,'Typhoid':18,'hepatitis A':19,
+'Hepatitis B':20,'Hepatitis C':21,'Hepatitis D':22,'Hepatitis E':23,'Alcoholic hepatitis':24,'Tuberculosis':25,
+'Common Cold':26,'Pneumonia':27,'Dimorphic hemmorhoids(piles)':28,'Heart attack':29,'Varicose veins':30,'Hypothyroidism':31,
+'Hyperthyroidism':32,'Hypoglycemia':33,'Osteoarthristis':34,'Arthritis':35,
+'(vertigo) Paroymsal  Positional Vertigo':36,'Acne':37,'Urinary tract infection':38,'Psoriasis':39,
+'Impetigo':40}},inplace=True)
+X = df[l1]
+y = df[["prognosis"]]
+
+gnb = MultinomialNB()
+gnb = gnb.fit(X, np.ravel(y))
+y_pred = gnb.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)*100
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the symptoms selected by the user from the form
+        symptom1 = request.form['symptom1']
+        symptom2 = request.form['symptom2']
+        symptom3 = request.form['symptom3']
+        symptom4 = request.form['symptom4']
+        symptom5 = request.form['symptom5']
+
+        # Create the symptom list
+        psymptoms = [symptom1, symptom2, symptom3, symptom4, symptom5]
+
+        # Initialize l2 list
+        l2 = [0] * len(l1)
+
+        # Update l2 based on selected symptoms
+        for k in range(len(l1)):
+            for z in psymptoms:
+                if z == l1[k]:
+                    l2[k] = 1
+
+        # Create inputtest for prediction
+        inputtest = [l2]
+
+        # Perform prediction using the Naive Bayes model
+        predicted = gnb.predict(inputtest)[0]
+
+        # Determine the disease name
+        predicted_disease = disease[predicted]
+
+        # Render the result template with the predicted disease and accuracy
+        return render_template('result.html', disease=predicted_disease, accuracy=accuracy)  # You can replace "N/A" with actual accuracy if available
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
